c9bd7be2d78acf32b9ca7b9bc9b93396345de214
Yann Klein <yann.klein@me.com>
2024-01-17 23:16:08 +0900

    refactor background.js to detect only the first m3u8 file

--- extension/background.js
+++ extension/background.js
@@ -1,32 +1,13 @@
-console.log("Hi from chrome extension");
-// 117017-004-A_v720.mp4
-// 117017-004-A_aud_VF-FRA_1.mp4
 function logURL(requestDetails) {
-    if (requestDetails.url.includes("A_v720.mp4")) {
-        // console.log(`Loading video: ${requestDetails.url}`);
-        const videoUrl = requestDetails.url;
-        document.dispatchEvent(new CustomEvent("video", { detail: videoUrl }));
-    } else if (requestDetails.url.includes("A_aud_VF-FRA_1.mp4")) {
-        // console.log(`Loading audio: ${requestDetails.url}`);
-        const audioUrl = requestDetails.url;
-        document.dispatchEvent(new CustomEvent("audio", { detail: audioUrl }));
-    } else {
-        return;
-    }
+  if (!caught_file && requestDetails.url.includes(".m3u8")) {
+    const videoUrl = requestDetails.url;
+    console.log(videoUrl);
+    chrome.webRequest.onBeforeRequest.removeListener(logURL);
+  }
 }
 
-chrome.webRequest.onBeforeRequest.addListener(logURL, {
-urls: ["<all_urls>"],
-});
+// initialize catch variable to "not caught yet"
+let caught_file = false;
 
-const videoPromise = new Promise((resolve, reject) => {
-    document.addEventListener("video", (e) => resolve(e.detail))
-});
-const audioPromise = new Promise((resolve, reject) => {
-    document.addEventListener("audio", (e) => resolve(e.detail))
-});
-
-Promise.all([videoPromise, audioPromise]).then((urls) => {
-  const [videoUrl, audioUrl] = urls;
-  console.log(videoUrl, audioUrl, urls);
-});
+// listen to all http responses
+chrome.webRequest.onBeforeRequest.addListener(logURL, {urls: ["<all_urls>"]});
