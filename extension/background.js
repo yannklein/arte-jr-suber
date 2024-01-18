ad36b9db4fd10875c9a7a949eb17db7fb4a4a312
Yann Klein <yann.klein@me.com>
2024-01-18 17:05:58 +0900

    define backend url accordin to env type

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,11 @@
+const isProdMode = 'update_url' in chrome.runtime.getManifest()
+const BACKEND_URL =  isProdMode ? 'TO BE DEFINED' : 'http://127.0.0.1:5000';
+
+
 function logURL(requestDetails) {
   if (!caught_file && requestDetails.url.includes(".m3u8")) {
     const videoUrl = requestDetails.url;
-    console.log(videoUrl);
+    console.log(videoUrl, BACKEND_URL);
     chrome.webRequest.onBeforeRequest.removeListener(logURL);
   }
 }
