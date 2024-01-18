2ce20a821d38a2aefb381c34bfbd78b44419dd42
Yann Klein <yann.klein@me.com>
2024-01-18 21:33:40 +0900

    add audio to text translation feature

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request, send_from_directory
 
 from modules.stream_to_video import stream_to_video
+from modules.audio_to_text import audio_to_text
 
 
 app = Flask(__name__)
@@ -12,16 +13,25 @@ app = Flask(__name__)
 def process_video():
     
     # Step0: cleanup the videos folder
-    os.system('rm -rf ./videos/*')
+    # os.system('rm -rf ./videos/*')
 
     # Step1: get video from stream url
-    stream_url = request.args.get("url")
-    original_urls = stream_to_video(stream_url)
+    # stream_url = request.args.get("url")
+    # original_urls = stream_to_video(stream_url)
     
     # Step2: get video transcript
+    original_urls = [
+        "http://127.0.0.1:5000/videos/original.mp4",
+        "http://127.0.0.1:5000/videos/original.mp3"
+    ]
+    stream_url = "https://manifest.arte.tv/api/manifest/v1/Generate/240117202245/fr/XQ/117014-013-A.m3u8"
+    
+    transcript = audio_to_text(original_urls[1])
+    
     outupt = {
         'original_vid_url': original_urls,
-        'stream_url': stream_url
+        'stream_url': stream_url,
+        'transcript': transcript
     }
     return json.dumps(outupt)
 
---
+++ server/modules/audio_to_text.py
@@ -0,0 +1,23 @@
+import os, json
+import whisper
+
+def audio_to_text(input_audio):
+
+    # Specify the directories
+    transcript_file = "videos/transcript.json"
+
+    print(transcript_file, input_audio)
+    # Load the whisper model
+    model = whisper.load_model("medium")
+    
+    # Transcribe the audio file
+    result = model.transcribe(input_audio)
+    # print(result)
+    
+    json_object = json.dumps(result, indent=4)
+
+    # Write the transcription to the output file
+    with open(transcript_file, 'w') as f:
+        f.write(json_object)
+    
+    return result
