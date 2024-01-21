b36928721243e6179c8d27b6cae67ee97290cfeb
Yann Klein <yann.klein@me.com>
2024-01-21 17:59:09 +0900

    build subtitle generation step and integrate every step together

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
-import os
-import json
+import os, json, time
 
 from flask import Flask, request, send_from_directory
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 from modules.stream_to_video import stream_to_video
 from modules.audio_to_text import audio_to_text
@@ -10,7 +9,7 @@ from modules.text_translation import text_translation
 from modules.generate_subtitle import generate_subtitle
 
 
-load_dotenv()
+# load_dotenv()
 # print(os.environ.get('PROJECTID'))
 
 app = Flask(__name__)
@@ -18,39 +17,43 @@ app = Flask(__name__)
 # main route to perform video full process
 @app.route('/')
 def process_video():
+    duration = {'start': time.time()}
     
     # Step0: cleanup the videos folder
-    # os.system('rm -rf ./videos/*')
+    os.system('rm -rf ./videos/*')
 
     # Step1: get video from stream url
-    # stream_url = request.args.get("url")
-    # original_urls = stream_to_video(stream_url)
+    stream_url = request.args.get("url")
+    original_urls = stream_to_video(stream_url)
+    duration['stream_to_video'] = time.time()
     
-    original_urls = [
-        "http://127.0.0.1:5000/videos/original.mp4",
-        "http://127.0.0.1:5000/videos/original.mp3"
-    ]
-    stream_url = "https://manifest.arte.tv/api/manifest/v1/Generate/240117202245/fr/XQ/117014-013-A.m3u8"
+    # original_urls = [
+    #     "http://127.0.0.1:5000/videos/original.mp4",
+    #     "http://127.0.0.1:5000/videos/original.mp3"
+    # ]
+    # stream_url = "https://manifest.arte.tv/api/manifest/v1/Generate/240117202245/fr/XQ/117014-013-A.m3u8"
     
     # Step2: get video transcript
-    # transcript = audio_to_text(original_urls[1])
-    
-    transcript = "http://127.0.0.1:5000/videos/transcript.json"
+    transcript = audio_to_text(original_urls[1])
+    duration['audio_to_text'] = time.time()
+    # transcript = "videos/transcript.json"
     
     # Step3: translate transcript
-    translations = text_translation(transcript, 'en')
-    
-    # translation = "http://127.0.0.1:5000/videos/translation.json"
+    translations = text_translation(f'http://127.0.0.1:5000/{transcript}', 'en')
+    duration['text_translation'] = time.time()
+    # translations = ["videos/translation.json", "videos/translation.srt"]
     
     # Step4: generate video subtitles
     final_video = generate_subtitle(translations[1], original_urls[0])
-    
+    duration['generate_subtitle'] = time.time()
+    print(duration)
     output = {
         'original_vid_url': original_urls,
         'stream_url': stream_url,
         'transcript': transcript,
         'translation': translations,
-        'final_video': final_video
+        'final_video': final_video,
+        'duration': duration
     }
     return json.dumps(output)
 
--- server/modules/stream_to_video.py
+++ server/modules/stream_to_video.py
@@ -4,10 +4,10 @@ def stream_to_video(stream_url):
     original_vid_url = 'videos/original.mp4'
     original_aud_url = 'videos/original.mp3'
     in_video = ffmpeg.input(stream_url, protocol_whitelist="file,http,https,tcp,tls,crypto")
-    out_video = ffmpeg.output(in_video, original_vid_url, vcodec="copy", acodec="copy")
     out_audio = ffmpeg.output(in_video, original_aud_url, acodec="libmp3lame")
-    out_video.run()
     out_audio.run()
+    out_video = ffmpeg.output(in_video, original_vid_url, vcodec="copy", acodec="copy")
+    out_video.run()
     return [
         f'http://127.0.0.1:5000/{original_vid_url}', 
         f'http://127.0.0.1:5000/{original_aud_url}'
