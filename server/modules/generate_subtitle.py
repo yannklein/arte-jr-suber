65e35521d394a83b55187e7cd12c45be2a9de76e
Yann Klein <yann.klein@me.com>
2024-01-22 10:14:34 +0900

    modify subbing step to add audio to final video

--- server/modules/generate_subtitle.py
+++ server/modules/generate_subtitle.py
@@ -3,7 +3,8 @@ import ffmpeg
 def generate_subtitle(translation_url, video_url):
     translated_url = 'videos/translated.mp4'
 
-    in_video = ffmpeg.input(video_url).filter("subtitles", translation_url)
-    out_video = ffmpeg.output(in_video, translated_url, vcodec="copy", acodec="copy")
-    out_video.run()
+    in_video = ffmpeg.input(video_url)
+    ffmpeg.filter(in_video, "subtitles", translation_url).output(in_video.audio, translated_url).run()
     return translated_url
+
+# generate_subtitle('/Users/kleinyann/github/arte-jr-suber/server/videos/translation.srt','/Users/kleinyann/github/arte-jr-suber/server/videos/original.mp4')
