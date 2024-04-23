458117def342de32e6cf99cfb8c469b2ea096d66
Yann Klein <yann.klein@me.com>
2024-04-23 19:48:50 +0900

    add terminal message when vid/aud downloaded

--- server/modules/stream_to_video.py
+++ server/modules/stream_to_video.py
@@ -10,9 +10,9 @@ def stream_to_video(stream_url):
     in_video = ffmpeg.input(stream_url, protocol_whitelist="file,http,https,tcp,tls,crypto")
     out_audio = ffmpeg.output(in_video, original_aud_url, acodec="libmp3lame", loglevel="quiet")
     out_audio.run()
-    print(f"{ftime()}: Audio downloaded! Check it out: {original_vid_url}")
+    print(f"{ftime()}: Audio downloaded! Check it out: {original_aud_url}")
     out_video = ffmpeg.output(in_video, original_vid_url, vcodec="copy", acodec="copy", loglevel="quiet")
     out_video.run()
-    print(f"{ftime()}: Video downloaded! Check it out: {original_aud_url}")
+    print(f"{ftime()}: Video downloaded! Check it out: {original_vid_url}")
     
     pass
